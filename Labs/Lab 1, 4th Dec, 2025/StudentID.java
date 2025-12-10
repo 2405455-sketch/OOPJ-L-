92f621c60ed442d00becf10324c799c21be727b3
2405455-sketch <2405455@kiit.ac.in>
2025-12-11 01:31:38 +0530

    2 assignments done

---
+++ Labs/Lab 1, 4th Dec, 2025/StudentID.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class StudentID {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter Name:");
+        String name=sc.nextLine();
+
+        System.out.println("Enter Branch:");
+        String branch=sc.nextLine();
+
+        System.out.println("Enter Roll No:");
+        int roll=sc.nextInt();
+
+        System.out.println("Enter Section: CSE-");
+        int section=sc.nextInt();
+
+        System.out.println("Name:"+name+"\nRoll no:"+roll+"\nSection:"+section+"\nBranch:"+branch);
+    }
+}
